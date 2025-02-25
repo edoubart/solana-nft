f204192fecfd818e5582351092109d7b04d5e134
edoubart <edouard.barthelemy@gmail.com>
2025-02-25 18:57:53 +0100

    Add boilerplate.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,14 @@ import './index.css';
 function App() {
   return (
     <div className="App">
+			<div className="container">
+				<div className="header-container">
+					<p className="header">🍭 Candy Drop</p>
+					<p className="sub-text">NFT drop machine with fair mint</p>
+				</div>
+				<div className="footer-container">
+				</div>
+			</div>
     </div>
   );
 }
